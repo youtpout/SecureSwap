45eac43211de866aa4cee2b1ecbff02ee0b3c97f
Youtpout <EddyBoughioul@hotmail.fr>
2023-07-27 14:23:24 +0200

    error type in transfer helper

--- contracts/libraries/TransferHelper.sol
+++ contracts/libraries/TransferHelper.sol
@@ -7,15 +7,20 @@ pragma solidity =0.8.4;
 
 // helper methods for interacting with ERC20 tokens and sending ETH that do not consistently return true/false
 library TransferHelper {
+    error ApproveFailed();
+    error TransferFailed();
+    error ETHTransferFailed();
+    error TransferFromFailed();
+
     function safeApprove(address token, address to, uint256 value) internal {
         // bytes4(keccak256(bytes('approve(address,uint256)')));
         (bool success, bytes memory data) = token.call(
             abi.encodeWithSelector(0x095ea7b3, to, value)
         );
-        require(
-            success && (data.length == 0 || abi.decode(data, (bool))),
-            "TransferHelper::safeApprove: approve failed"
-        );
+
+        if (!success || (data.length > 0 && !abi.decode(data, (bool)))) {
+            revert ApproveFailed();
+        }
     }
 
     function safeTransfer(address token, address to, uint256 value) internal {
@@ -23,10 +28,9 @@ library TransferHelper {
         (bool success, bytes memory data) = token.call(
             abi.encodeWithSelector(0xa9059cbb, to, value)
         );
-        require(
-            success && (data.length == 0 || abi.decode(data, (bool))),
-            "TransferHelper::safeTransfer: transfer failed"
-        );
+        if (!success || (data.length > 0 && !abi.decode(data, (bool)))) {
+            revert TransferFailed();
+        }
     }
 
     function safeTransferFrom(
@@ -39,17 +43,15 @@ library TransferHelper {
         (bool success, bytes memory data) = token.call(
             abi.encodeWithSelector(0x23b872dd, from, to, value)
         );
-        require(
-            success && (data.length == 0 || abi.decode(data, (bool))),
-            "TransferHelper::transferFrom: transferFrom failed"
-        );
+        if (!success || (data.length > 0 && !abi.decode(data, (bool)))) {
+            revert TransferFromFailed();
+        }
     }
 
     function safeTransferETH(address to, uint256 value) internal {
         (bool success, ) = to.call{value: value}(new bytes(0));
-        require(
-            success,
-            "TransferHelper::safeTransferETH: ETH transfer failed"
-        );
+        if (!success) {
+            revert ETHTransferFailed();
+        }
     }
 }
